7d86a2d6b1e7ac59a92eeabaa20143dcfb3e457f
KingWin <kingweiliu@gmail.com>
2014-01-09 20:23:40 +0800

    test async protocol

--- LjwBrowser/AyncPlugginableProtocol.cpp
+++ LjwBrowser/AyncPlugginableProtocol.cpp
@@ -11,13 +11,29 @@ CAyncPlugginableProtocol::~CAyncPlugginableProtocol(void)
 {
 }
 
+//The URL moniker calls this method before it sends an HTTP request. It notifies the client of the URL that is being bound to at the beginning 
+//of the HTTP transaction. It also enables the client to add additional headers, such as Accept-Language, to the request.
+//每一个网络请求都经过这个函数, 可以通过这个过滤请求。 
 // IHttpNegotiate
+//
 STDMETHODIMP CAyncPlugginableProtocol::CMyProtocolSink::BeginningTransaction(
 	/* [in] */ LPCWSTR szURL,
 	/* [in] */ LPCWSTR szHeaders,
 	/* [in] */ DWORD dwReserved,
 	/* [out] */ LPWSTR *pszAdditionalHeaders){
-		return S_OK;
+        
+    OutputDebugString(CString(szURL) + "\n");
+    CString strUrl = szURL;
+    if (-1 != strUrl.Find(L".jpg") || -1 != strUrl.Find(L".gif") || -1!=strUrl.Find(L".png"))
+    {
+        return E_ABORT;
+    }
+
+    //增加header; 
+    WCHAR* szNewHeader = (LPWSTR)CoTaskMemAlloc(1024 * sizeof(WCHAR));
+    memset(szNewHeader, 0, sizeof(WCHAR)*1024);
+    *pszAdditionalHeaders = szNewHeader;
+	return S_OK;
 }
 
 STDMETHODIMP CAyncPlugginableProtocol::CMyProtocolSink::OnResponse(
@@ -26,6 +42,7 @@ STDMETHODIMP CAyncPlugginableProtocol::CMyProtocolSink::OnResponse(
 	/* [in] */ LPCWSTR szRequestHeaders,
 	/* [out] */ LPWSTR *pszAdditionalRequestHeaders){
 		return S_OK;
+        
 }
 
 STDMETHODIMP CAyncPlugginableProtocol::CMyProtocolSink::ReportProgress(
